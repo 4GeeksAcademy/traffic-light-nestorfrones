aa2123cc68e70c26771e63c68575598e63b96b51
Nestor Frones <115091611+Nestorfron@users.noreply.github.com>
2024-05-18 06:47:17 +0000

    add purple

--- src/js/component/TrafficLight.jsx
+++ src/js/component/TrafficLight.jsx
@@ -8,40 +8,101 @@ const TrafficLight = () => {
     const [green, setGreen] = useState(true);
     const [yellow, setYellow] = useState(false);
     const [red, setRed] = useState(false);
+    
+    const [purple, setPurple] = useState(false);
 
+    const [newColor, setNewColor] = useState(false);
+
+
+    function addNewColor (){
+        if (newColor === false)
+            setNewColor(true)
+            setColor("purple")
+        if (newColor === true)
+            setNewColor(false)
+            setColor("red")
+    }
+ 
 
     function setColor(color) {
-        if (color === "green") {
-            setGreen(true)
-            setYellow(false)
-            setRed(false)
+        if (newColor === true) {
+            if (color === "purple") {
+                setPurple(true)
+                setGreen(false)
+                setYellow(false)
+                setRed(false)
+            }
+            if (color === "green") {
+                setGreen(true)
+                setYellow(false)
+                setRed(false)
+                setPurple(false)
+            }
+            if (color === "yellow") {
+                setYellow(true)
+                setGreen(false)
+                setRed(false)
+                setPurple(false)
+            }
+            if (color === "red") {
+                setRed(true)
+                setGreen(false)
+                setYellow(false)
+                setPurple(false)
+            }
         }
-        if (color === "yellow") {
-            setYellow(true)
-            setGreen(false)
-            setRed(false)
+
+        if(newColor === false) {
+            if (color === "green") {
+                setGreen(true)
+                setYellow(false)
+                setRed(false)
+            }
+            if (color === "yellow") {
+                setYellow(true)
+                setGreen(false)
+                setRed(false)
+            }
+            if (color === "red") {
+                setRed(true)
+                setGreen(false)
+                setYellow(false)
+            }
         }
-        if (color === "red") {
-            setRed(true)
-            setGreen(false)
-            setYellow(false)
-        }
+       
 
     }
 
 
     function changeLight() {
-        
-        if (green === true ){
-            setColor("yellow")
-        }    
-        if (yellow === true){
-            setColor("red")
+        if (newColor === true) {
+            if (green === true ){
+                setColor("yellow")
+            }    
+            if (yellow === true){
+                setColor("red")
+            }
+            if (red === true){
+                setColor("purple")
+            }
+            if (purple === true){
+                setColor("green")
+            }
         }
-        if (red === true){
-            setColor("green")
+        if (newColor === false) {
+            if (green === true ){
+                setColor("yellow")
+            }    
+            if (yellow === true){
+                setColor("red")
+            }
+            if (red === true){
+                setColor("green")
+            }
         }
     }
+
+    
     
 
     return (
@@ -49,7 +110,10 @@ const TrafficLight = () => {
             <button onClick={() => setColor("red")} className={`btn btn-danger ${!red && "opacity-25"}`}>rojo</button>
             <button onClick={() => setColor("yellow")} className={`btn btn-warning ${!yellow && "opacity-25"}`}>amarillo</button>
             <button onClick={() => setColor("green")} className={`btn btn-success ${!green && "opacity-25"}`}>verde</button>
+            <button onClick={() => setColor("purple")} className={`btn btn-success ${!purple && "opacity-25"}`}>purpura</button>
             <button onClick={() => changeLight()} className="btn btn-success">Change Light</button>
+            <button onClick={() => addNewColor()} className="btn btn-success">Add Purple</button>
+
         </div>
     )
 
