bbba2d9f7dc0974d41d415717bdbbbcdde643389
Nestor Frones <115091611+Nestorfron@users.noreply.github.com>
2024-05-18 04:56:12 +0000

    add button

--- src/js/component/TrafficLight.jsx
+++ src/js/component/TrafficLight.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 
-// let light = "red"
+
 
 
 const TrafficLight = () => {
@@ -11,33 +11,48 @@ const TrafficLight = () => {
 
 
     function setColor(color) {
-       if (color === "green") {
+        if (color === "green") {
             setGreen(true)
             setYellow(false)
             setRed(false)
-       }
-       if (color === "yellow") {
+        }
+        if (color === "yellow") {
             setYellow(true)
             setGreen(false)
             setRed(false)
-       }
-       if (color === "red") {
+        }
+        if (color === "red") {
             setRed(true)
             setGreen(false)
             setYellow(false)
-       }  
+        }
+
     }
 
 
+    function changeLight() {
+        
+        if (green === true ){
+            setColor("yellow")
+        }    
+        if (yellow === true){
+            setColor("red")
+        }
+        if (red === true){
+            setColor("green")
+        }
+    }
+    
+
     return (
         <div>
-            <button onClick={() => setColor("red")} className={`btn btn-danger ${!red && "opacity-25" }`}>rojo</button>
-            <button onClick={() => setColor("yellow")} className={`btn btn-warning ${!yellow && "opacity-25" }`}>amarillo</button>
-            <button onClick={() => setColor("green")} className={`btn btn-success ${!green && "opacity-25" }`}>verde</button>
+            <button onClick={() => setColor("red")} className={`btn btn-danger ${!red && "opacity-25"}`}>rojo</button>
+            <button onClick={() => setColor("yellow")} className={`btn btn-warning ${!yellow && "opacity-25"}`}>amarillo</button>
+            <button onClick={() => setColor("green")} className={`btn btn-success ${!green && "opacity-25"}`}>verde</button>
+            <button onClick={() => changeLight()} className="btn btn-success">Change Light</button>
         </div>
     )
 
 
-}   
-
+        }
 export default TrafficLight;
